65141acda7cc2c6c05e32dc3e8e1de9a49aff5e5
alphacore397-cmd <alphacore397@gmail.com>
2026-07-07 01:36:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const counters = document.querySelectorAll(".exp-card h2");
 
+
+counters.forEach(counter=>{
+
+let target=parseInt(counter.innerText);
+
+let count=0;
+
+
+let speed=target/80;
+
+
+let update=()=>{
+
+if(count<target){
+
+count+=speed;
+
+counter.innerText=Math.ceil(count)+"+";
+
+requestAnimationFrame(update);
+
+}
+
+else{
+
+counter.innerText=target+"+";
+
+}
+
+};
+
+
+update();
+
+
+});
